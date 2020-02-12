89234b0974acc0f12915cd2eb613f908bf12a2e4
yonghui-cc <cpu_yonghui@163.com>
2020-02-12 14:09:26 +0100

    update util.py with doc'

--- molpy/util.py
+++ molpy/util.py
@@ -1,6 +1,20 @@
 import numpy as np
 
+
 def distance(point1, point2):
+    """
+    Calculate distance between two points
+
+    parameters
+    ---------
+    point1 : array_like
+    point2 : array_like
+
+    Return:
+    --------
+    float
+        The distance between point1 and point2.
+    """
     point1 = np.asarray(point1)
     point2 = np.asarray(point2)
     return np.linalg.norm(point1 - point2)
