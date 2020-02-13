e3f0d3354a3d12dec76bcee12c174e69aa030ee8
yonghui-cc <cpu_yonghui@163.com>
2020-02-13 09:35:09 +0100

    update homework 2

--- molpy/data/read_xyz_files.py
+++ molpy/data/read_xyz_files.py
@@ -23,9 +23,13 @@ def open_xyz(molecule):
     filename = os.path.join(dirname, molecule+'.xyz')      
     xyz_file = np.genfromtxt(fname=filename, skip_header=2, dtype='unicode')
     symbols = list(xyz_file[:,0])
-    coords = (xyz_file[:,1:])
-    coords = list(coords.astype(np.float))
-
+    
+#    coords = xyz_file[:,1:].astype(np.float)
+    coords = xyz_file[:,1:]
+#    
+    coords = coords.astype(np.float)
+    coords = list(np.mean(coords, axis=0))
     return symbols, coords
 
 
+print (open_xyz('water'))
--- molpy/tests/test_read_xyz_files.py
+++ molpy/tests/test_read_xyz_files.py
@@ -1,20 +1,21 @@
 import pytest
 import molpy
-import numpy as np
+#import numpy as np
 
 @pytest.mark.parametrize("molecule, symbols, coords",[
     ('water',
      ['H', 'H', 'O'],
-     [[np.array([ 9.626,  6.787, 12.673]), 
-       np.array([ 9.626,  8.42 , 12.673]), 
-       np.array([10.203,  7.604, 12.673])]])
+     [9.818333333333333, 7.603666666666666, 12.673])
     ])
 
 
 def test_read_xyz_files(molecule, symbols, coords):
 
     assert molpy.data.open_xyz(molecule)[0] == symbols
-#    assert molpy.data.open_xyz(molecule)[1] == coords
+    
+    for i in range(0,3):
+        assert molpy.data.open_xyz(molecule)[1][i] \
+            == pytest.approx(coords[i], abs=1.e-3)
     
     
 
